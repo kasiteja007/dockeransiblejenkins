34ece94e2790aedceb59fa2a4270d6668bccf9f1
kasiteja007 <100054986+kasiteja007@users.noreply.github.com>
2022-07-16 17:31:32 +0530

    Create vpcandprivatesubnet.tf

---
+++ vpcandprivatesubnet.tf
@@ -0,0 +1,28 @@
+resource "aws_vpc" "ecs_vpc" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = "VPC for ecs"
+  }
+}
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.ecs_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "private subnet 1"
+  }
+}
+
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.ecs_vpc.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "private subnet 2"
+  }
+}
